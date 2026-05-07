1fbc5c225b1de43cd5d53fbbcbbf3bfc0dd5265f
GO <goierj@gmail.com>
2026-05-07 12:04:57 +0300

    2-ое задание 1-й практической

---
+++ Practice-1.3.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program{
+	static void Main(){
+		string input;
+		int target;
+		int[] nums;
+		
+		Console.Write("Введите элементы массива: ");
+		input = Console.ReadLine();
+		nums = Array.ConvetAll(input.Split(' '), int.Parse);
+
+		Console.Write("Введите искомое число: ");
+		target = Console.ReadLine();
+		
+		
+
+		for(int i; i < nums.size()){
+			
+		}
+
+	}
+
+}
