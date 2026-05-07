85f042bfe2c37e59bdb364456d8f5512ee04a70d
GO <goierj@gmail.com>
2026-05-07 12:33:13 +0300

    3 : 1

--- Practice-1.3.cs
+++ Practice-1.3.cs
@@ -5,19 +5,25 @@ class Program{
 		string input;
 		int target;
 		int[] nums;
+		int size;
 		
 		Console.Write("Введите элементы массива: ");
 		input = Console.ReadLine();
 		nums = Array.ConvetAll(input.Split(' '), int.Parse);
+		size = nums.Lenght();
 
 		Console.Write("Введите искомое число: ");
 		target = Console.ReadLine();
 		
 		
 
-		for(int i; i < nums.size()){
-			
-		}
+		for(int i = 0; i < size, i++){
+			for(int j = i + 1; j < size, j++){
+				if(nums[i] + nums[j] == target)
+					Console.WriteLine($"{i}, {j}");
+			}
+
+		}	
 
 	}
 
