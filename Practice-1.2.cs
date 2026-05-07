bf99aa40dd14bf136f7dc76fff0f56aec9bd21ce
GO <goierj@gmail.com>
2026-05-07 12:51:47 +0300

    2 : 1

--- Practice-1.2.cs
+++ Practice-1.2.cs
@@ -1,29 +1,33 @@
 using System;
 using System.Collections.Generic;
 
-class Program {
+class Program
+{
 
-    static void main(){
+    static void main()
+    {
         string input;
         int[] nums;
-        
+
         Console.Write("Введите элементы массива: ");
         input = Console.ReadLine();
 
         //Преобразуем строковые значения в целочисленные
-        int[] nums = Array.ConvertAll(input.Slice(' '), int.Parse);
+        Array.ConvertAll(input.Splite(' '), int.Parse);
 
         //Функция возвращает true или false
         bool Dup = CheckDuplicates(nums);
         Console.WriteLine(Dup ? "Повторяющиеся элементы присутствуют" : "Повторяющиихся элементов нет");
     }
 
-    static bool CheckDuplicates(int[] nums){
+    static bool CheckDuplicates(int[] nums)
+    {
         HashSet<int> array = new HashSet<int>();
 
         //Перебираем элементы словаря, используя итератор "c"
-        foreach(int c in array){
-            if(array.Contains(c)) //Возвращает true в случае идентичного элемента
+        foreach (int c in array)
+        {
+            if (array.Contains(c)) //Возвращает true в случае идентичного элемента
                 return true; // Дубликант найден. Производим возврат из функции
 
             array.Add(c); //Добавляем элемент в наш HashSet
